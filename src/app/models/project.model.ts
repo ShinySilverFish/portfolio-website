b09b71ecc4712aa395b2a9360415b581f22a6c1a
Rynhard <rpietersen007@gmail.com>
2023-05-02 13:24:51 +0200

    Added link to github on projects page

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -3,7 +3,7 @@ export const PROJECTS = [
         name: 'Portfolio Website',
         description: 'A website I wrote to showcase my skills in angular and to act as a hub to showcase any other projects I worked on.',
         images: ['../../assets/images/portfolio-website/screenshot1.jpg', '../../assets/images/portfolio-website/screenshot2.jpg', '../../assets/images/portfolio-website/screenshot3.jpg', '../../assets/images/portfolio-website/screenshot4.jpg'],
-        githubLink: 'poopyscoopy',
+        githubLink: 'https://github.com/ShinySilverFish/portfolio-website',
         tags: ['Angular', 'TypeScript', 'SCSS'],
     },
 ]
